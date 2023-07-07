13b42a88723a094018841c1df827db35f0f4bf76
GAWE IT <67357646+gaweit@users.noreply.github.com>
2023-07-07 22:19:27 +0700

    woke

--- pos-kasir-php-master/db_toko.sql
+++ pos-kasir-php-master/db_toko.sql
@@ -1,21 +1,24 @@
-/*
-SQLyog Professional v12.5.1 (64 bit)
-MySQL - 10.6.11-MariaDB-log : Database - db_toko
-*********************************************************************
-*/
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.7.33 - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.2.0.6213
+-- --------------------------------------------------------
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*Table structure for table `barang` */
 
-DROP TABLE IF EXISTS `barang`;
 
-CREATE TABLE `barang` (
+-- Dumping database structure for db_toko
+CREATE DATABASE IF NOT EXISTS `db_toko` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `db_toko`;
+
+-- Dumping structure for table db_toko.barang
+CREATE TABLE IF NOT EXISTS `barang` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_barang` varchar(255) NOT NULL,
   `id_kategori` int(11) NOT NULL,
@@ -28,56 +31,55 @@ CREATE TABLE `barang` (
   `tgl_input` varchar(255) NOT NULL,
   `tgl_update` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
-/*Data for the table `barang` */
+-- Dumping data for table db_toko.barang: ~3 rows (approximately)
+/*!40000 ALTER TABLE `barang` DISABLE KEYS */;
+INSERT INTO `barang` (`id`, `id_barang`, `id_kategori`, `nama_barang`, `merk`, `harga_beli`, `harga_jual`, `satuan_barang`, `stok`, `tgl_input`, `tgl_update`) VALUES
+	(1, 'BR001', 1, 'Pensil', 'Fabel Castel', '1500', '3000', 'PCS', '98', '6 October 2020, 0:41', NULL);
+INSERT INTO `barang` (`id`, `id_barang`, `id_kategori`, `nama_barang`, `merk`, `harga_beli`, `harga_jual`, `satuan_barang`, `stok`, `tgl_input`, `tgl_update`) VALUES
+	(2, 'BR002', 5, 'Sabun', 'Lifeboy', '1800', '3000', 'PCS', '38', '6 October 2020, 0:41', '6 October 2020, 0:54');
+INSERT INTO `barang` (`id`, `id_barang`, `id_kategori`, `nama_barang`, `merk`, `harga_beli`, `harga_jual`, `satuan_barang`, `stok`, `tgl_input`, `tgl_update`) VALUES
+	(3, 'BR003', 1, 'Pulpen', 'Standard', '1500', '2000', 'PCS', '70', '6 October 2020, 1:34', NULL);
+/*!40000 ALTER TABLE `barang` ENABLE KEYS */;
 
-insert  into `barang`(`id`,`id_barang`,`id_kategori`,`nama_barang`,`merk`,`harga_beli`,`harga_jual`,`satuan_barang`,`stok`,`tgl_input`,`tgl_update`) values 
-(1,'BR001',1,'Pensil','Fabel Castel','1500','3000','PCS','98','6 October 2020, 0:41',NULL),
-(2,'BR002',5,'Sabun','Lifeboy','1800','3000','PCS','38','6 October 2020, 0:41','6 October 2020, 0:54'),
-(3,'BR003',1,'Pulpen','Standard','1500','2000','PCS','70','6 October 2020, 1:34',NULL);
-
-/*Table structure for table `kategori` */
-
-DROP TABLE IF EXISTS `kategori`;
-
-CREATE TABLE `kategori` (
+-- Dumping structure for table db_toko.kategori
+CREATE TABLE IF NOT EXISTS `kategori` (
   `id_kategori` int(11) NOT NULL AUTO_INCREMENT,
   `nama_kategori` varchar(255) NOT NULL,
   `tgl_input` varchar(255) NOT NULL,
   PRIMARY KEY (`id_kategori`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
-/*Data for the table `kategori` */
+-- Dumping data for table db_toko.kategori: ~4 rows (approximately)
+/*!40000 ALTER TABLE `kategori` DISABLE KEYS */;
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `tgl_input`) VALUES
+	(1, 'ATK', '7 May 2017, 10:23');
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `tgl_input`) VALUES
+	(5, 'Sabun', '7 May 2017, 10:28');
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `tgl_input`) VALUES
+	(6, 'Snack', '6 October 2020, 0:19');
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `tgl_input`) VALUES
+	(7, 'Minuman', '6 October 2020, 0:20');
+/*!40000 ALTER TABLE `kategori` ENABLE KEYS */;
 
-insert  into `kategori`(`id_kategori`,`nama_kategori`,`tgl_input`) values 
-(1,'ATK','7 May 2017, 10:23'),
-(5,'Sabun','7 May 2017, 10:28'),
-(6,'Snack','6 October 2020, 0:19'),
-(7,'Minuman','6 October 2020, 0:20');
-
-/*Table structure for table `login` */
-
-DROP TABLE IF EXISTS `login`;
-
-CREATE TABLE `login` (
+-- Dumping structure for table db_toko.login
+CREATE TABLE IF NOT EXISTS `login` (
   `id_login` int(11) NOT NULL AUTO_INCREMENT,
   `user` varchar(255) NOT NULL,
   `pass` char(32) NOT NULL,
   `id_member` int(11) NOT NULL,
   PRIMARY KEY (`id_login`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
-/*Data for the table `login` */
+-- Dumping data for table db_toko.login: ~0 rows (approximately)
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+INSERT INTO `login` (`id_login`, `user`, `pass`, `id_member`) VALUES
+	(1, 'admin', '202cb962ac59075b964b07152d234b70', 1);
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
 
-insert  into `login`(`id_login`,`user`,`pass`,`id_member`) values 
-(1,'admin','202cb962ac59075b964b07152d234b70',1);
-
-/*Table structure for table `member` */
-
-DROP TABLE IF EXISTS `member`;
-
-CREATE TABLE `member` (
+-- Dumping structure for table db_toko.member
+CREATE TABLE IF NOT EXISTS `member` (
   `id_member` int(11) NOT NULL AUTO_INCREMENT,
   `nm_member` varchar(255) NOT NULL,
   `alamat_member` text NOT NULL,
@@ -86,18 +88,16 @@ CREATE TABLE `member` (
   `gambar` text NOT NULL,
   `NIK` text NOT NULL,
   PRIMARY KEY (`id_member`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
-/*Data for the table `member` */
+-- Dumping data for table db_toko.member: ~0 rows (approximately)
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+INSERT INTO `member` (`id_member`, `nm_member`, `alamat_member`, `telepon`, `email`, `gambar`, `NIK`) VALUES
+	(1, 'Total Buah', 'uj harapan', '081234567890', 'example@gmail.com', 'unnamed.jpg', '12314121');
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
 
-insert  into `member`(`id_member`,`nm_member`,`alamat_member`,`telepon`,`email`,`gambar`,`NIK`) values 
-(1,'Total-Buah.com','uj harapan','081234567890','example@gmail.com','unnamed.jpg','12314121');
-
-/*Table structure for table `nota` */
-
-DROP TABLE IF EXISTS `nota`;
-
-CREATE TABLE `nota` (
+-- Dumping structure for table db_toko.nota
+CREATE TABLE IF NOT EXISTS `nota` (
   `id_nota` int(11) NOT NULL AUTO_INCREMENT,
   `id_barang` varchar(255) NOT NULL,
   `id_member` int(11) NOT NULL,
@@ -106,15 +106,14 @@ CREATE TABLE `nota` (
   `tanggal_input` varchar(255) NOT NULL,
   `periode` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_nota`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `nota` */
+-- Dumping data for table db_toko.nota: ~0 rows (approximately)
+/*!40000 ALTER TABLE `nota` DISABLE KEYS */;
+/*!40000 ALTER TABLE `nota` ENABLE KEYS */;
 
-/*Table structure for table `penjualan` */
-
-DROP TABLE IF EXISTS `penjualan`;
-
-CREATE TABLE `penjualan` (
+-- Dumping structure for table db_toko.penjualan
+CREATE TABLE IF NOT EXISTS `penjualan` (
   `id_penjualan` int(11) NOT NULL AUTO_INCREMENT,
   `id_barang` varchar(255) NOT NULL,
   `id_member` int(11) NOT NULL,
@@ -122,28 +121,31 @@ CREATE TABLE `penjualan` (
   `total` varchar(255) NOT NULL,
   `tanggal_input` varchar(255) NOT NULL,
   PRIMARY KEY (`id_penjualan`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
-/*Data for the table `penjualan` */
+-- Dumping data for table db_toko.penjualan: ~0 rows (approximately)
+/*!40000 ALTER TABLE `penjualan` DISABLE KEYS */;
+INSERT INTO `penjualan` (`id_penjualan`, `id_barang`, `id_member`, `jumlah`, `total`, `tanggal_input`) VALUES
+	(1, 'BR003', 1, '1', '2000', '5 July 2023, 21:20');
+/*!40000 ALTER TABLE `penjualan` ENABLE KEYS */;
 
-/*Table structure for table `toko` */
-
-DROP TABLE IF EXISTS `toko`;
-
-CREATE TABLE `toko` (
+-- Dumping structure for table db_toko.toko
+CREATE TABLE IF NOT EXISTS `toko` (
   `id_toko` int(11) NOT NULL AUTO_INCREMENT,
   `nama_toko` varchar(255) NOT NULL,
   `alamat_toko` text NOT NULL,
   `tlp` varchar(255) NOT NULL,
   `nama_pemilik` varchar(255) NOT NULL,
   PRIMARY KEY (`id_toko`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
-/*Data for the table `toko` */
+-- Dumping data for table db_toko.toko: ~0 rows (approximately)
+/*!40000 ALTER TABLE `toko` DISABLE KEYS */;
+INSERT INTO `toko` (`id_toko`, `nama_toko`, `alamat_toko`, `tlp`, `nama_pemilik`) VALUES
+	(1, 'Toko Total Buah', 'Jakarta', '081234567890', 'Total Buah');
+/*!40000 ALTER TABLE `toko` ENABLE KEYS */;
 
-insert  into `toko`(`id_toko`,`nama_toko`,`alamat_toko`,`tlp`,`nama_pemilik`) values 
-(1,'CV Daruttaqwa','Ujung Harapan','081234567890','Total-Buah.com');
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
