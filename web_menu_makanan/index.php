d31cd94fd5cdfcca0dc423e0204415a93fbe7281
GAWE IT <67357646+gaweit@users.noreply.github.com>
2023-07-09 21:24:29 +0700

    makanan

--- web_menu_makanan/index.php
+++ web_menu_makanan/index.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
- 
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
- 
+
     <title>Order Page</title>
     <link rel="stylesheet" href="style.css">
 
 
 </head>
+
 <body>
     <?php
     session_start();
@@ -45,29 +47,29 @@
 
     <div class="container-input">
         <div class="form-container">
-            <form method="POST" action="index.php"  class="login-form">
-                
-            <h2>Order Form</h2>
-            <div>
+            <form method="POST" action="index.php" class="login-form">
+
+                <h2>Order Form</h2>
+                <div>
                     <label for="food_code">Food Code:</label>
-                    <input type="text" id="food_code" name="food_code" required>
+                    <input type="text" id="food_code" name="food_code" class="form-control" required>
                 </div>
                 <div>
                     <label for="food_name">Food Name:</label>
-                    <input type="text" id="food_name" name="food_name" required>
+                    <input type="text" id="food_name" name="food_name" class="form-control" required>
                 </div>
                 <div>
                     <label for="food_price">Food Price:</label>
-                    <input type="number" id="food_price" name="food_price" required>
+                    <input type="number" id="food_price" name="food_price" class="form-control" required>
                 </div>
                 <div>
                     <label for="food_photo_url">Food Photo URL:</label>
-                    <input type="text" id="food_photo_url" name="food_photo_url" required>
-                </div><button type="submit" >Add to Order</button>
-         </form>
-         <br>
-         <div align="center">
-<a href="order.php"><button type="">Page Order -></button></a> 
-     </div>
+                    <input type="text" id="food_photo_url" name="food_photo_url" class="form-control" required>
+                    <br>
+                </div><button type="submit" class="btn btn-primary">Simpan </button>
+            </form>
+            <br>
+            <div align="center">
+                <a href="order.php"><button type="" class="btn btn-primary">Page Order -></button></a>
+            </div>
         </div>
-
--- web_menu_makanan/order.php
+++ web_menu_makanan/order.php
@@ -1,84 +1,93 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
- 
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
- 
+
     <title>Order Page</title>
     <link rel="stylesheet" href="style.css">
 
 
 </head>
+
 <body>
 
     <div class="container">
         <div class="absolute">
-          <h1><b>Pilihanku:</b></h1>
- <div id="cartItems"></div>
- <div id="totalPrice"></div>
- 
-  <a href="index.php"><button>Input Menu</button></a> 
-<form method="POST" action="">
-<input type="submit" name="hapus" value="Hapus Session" >
-</form>
-  </div>
-<div class="row">
-<?php 
+            <h1><b>Pilihanku:</b></h1>
+            <div id="cartItems"></div>
+            <div id="totalPrice"></div>
+
+            <a href="index.php"><button class="btn btn-primary">Input Menu</button></a>
+            <br>
+            <br>
+            <br>
+            <form method="POST" action="">
+                <input type="submit" class="btn btn-primary" name="hapus" value="Hapus Session">
+            </form>
+        </div>
+        <div class="row">
+            <?php 
 
     session_start();
     if (!empty($_SESSION['order'])) {
   foreach ($_SESSION['order'] as $item) {?>
-<div class="card">
-  <img src="photo.jpg" alt="Avatar" style="width:100%">
-  <div class="container">
-    <h4><b><?= $item['name'];?></b></h4> 
-    <p><?= 'Rp. '.$item['price']?></p> 
-    <div align="center">
-    <button id="select-food" onclick="addToCart('<?= $item['name'];?>', <?=$item['price']?>)">pilih</button></div>
-  </div>
-</div>
-<?php }}?>
-</div>
-<div class="container">
+            <div class="card">
+                <img src="<?= $item['photo_url'];?>" alt="Avatar" style="width:100%">
+                <div class="container">
+                    <h4><b><?= $item['name'];?></b></h4>
+                    <p><?= 'Rp. '.$item['price']?></p>
+                    <div align="center">
+                        <button id="select-food" class="btn btn-warning"
+                            onclick="addToCart('<?= $item['name'];?>', <?=$item['price']?>)">Order Now </button>
+                    </div>
+                </div>
+            </div>
+            <?php }}?>
+        </div>
+        <div class="container">
 
-</div>
-<?php if (isset($_POST['hapus'])) {
+        </div>
+        <?php if (isset($_POST['hapus'])) {
  
 session_destroy();
 
 }?>
-</div>
+    </div>
 </body>
+
 </html>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-   
-   <script>
-        var shoppingCart = []; // Menyimpan data keranjang belanja
 
-        function addToCart(name, price) {
-            var item = {
-                name: name,
-                price: price
-            };
+<script>
+var shoppingCart = []; // Menyimpan data keranjang belanja
 
-            shoppingCart.push(item);
-            displayCart();
-        }
+function addToCart(name, price) {
+    var item = {
+        name: name,
+        // photo_url: photo_url,
+        price: price
+    };
 
-        function displayCart() {
-            var cartItems = "";
-            var totalPrice = 0;
+    shoppingCart.push(item);
+    displayCart();
+}
 
-            shoppingCart.forEach(function(item) {
-                cartItems += "<div>" + item.name + " (Rp." + item.price + ")</div>";
-                totalPrice += item.price;
-            });
+function displayCart() {
+    var cartItems = "";
+    var totalPrice = 0;
 
-            $("#cartItems").html(cartItems);
-            $("#totalPrice").text("Total: " + totalPrice);
-        }
-    </script>
+    shoppingCart.forEach(function(item) {
+        cartItems += "<div>" + item.name + " (Rp." + item.price + ")</div>";
+        totalPrice += item.price;
+    });
+
+    $("#cartItems").html(cartItems);
+    $("#totalPrice").text("Total: " + totalPrice);
+}
+</script>
